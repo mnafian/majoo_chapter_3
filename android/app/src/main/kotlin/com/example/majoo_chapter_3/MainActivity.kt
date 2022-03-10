bf007642a86e94e67acf5dbea8200735a2c9cd13
mnafian <nafian.wildana@bukalapak.com>
2022-03-10 19:50:01 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/majoo_chapter_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.majoo_chapter_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bottom_sheet.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetView extends StatefulWidget {
+  const BottomSheetView({Key? key}) : super(key: key);
+
+  @override
+  State<BottomSheetView> createState() => _BottomSheetViewState();
+}
+
+class _BottomSheetViewState extends State<BottomSheetView> {
+  @override
+  Widget build(BuildContext context) {
+    final paddingBottom = MediaQuery.of(context).viewInsets.bottom;
+    final _nameEditController = TextEditingController();
+    final _cityNameEditController = TextEditingController();
+
+    DateTime _selectedDate = DateTime.now();
+
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+            left: 10,
+            right: 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Input Nama'),
+                controller: _nameEditController,
+                onSubmitted: (_) => {},
+              ),
+
+              //TextField
+              TextField(
+                decoration: const InputDecoration(labelText: 'Input Kota'),
+                controller: _cityNameEditController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => {},
+              ),
+              SizedBox(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date: $_selectedDate',
+                      ), //Text
+                    ), //Expand
+                    TextButton(
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      child: const Text(
+                        'Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ), //TextStyle
+                      ), //Text
+                      onPressed: () {
+                        _presentDatePicker(context);
+                      },
+                    ), //FlatButton
+                  ], //<Widget>[]
+                ), //Row
+              ), //Container
+              ElevatedButton(
+                child: const Text('Submit'),
+                onPressed: () {},
+              ), //RaisedButton
+            ], //<Widget>[]
+          ),
+        ), //Column
+      ),
+    ); //Card
+  }
+
+  void _presentDatePicker(BuildContext context) {}
+}
